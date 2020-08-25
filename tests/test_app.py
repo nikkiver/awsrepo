cd0bbd6282212f45d002fa46b921d109192e352a
nikkiver <n.sharma234@gmail.com>
2020-08-25 21:40:26 +0530

    ADDED FUNCTIONAL TEST USING PYTEST

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,20 @@
 from  app.route import  index
+from app import  application
+import  pytest
 
 
 
-def test_index():
-    assert index() ==("Index page rendered" ,200)
+@pytest.fixture(scope='module')
+def test_client():
+    test_client = application.test_client()
+    ctx = application.app_context()
+    ctx.push()
+
+    yield test_client
+
+    ctx.pop()
+
+
+def test_index(test_client):
+    response = test_client.get('/')
+    assert response.status_code == 200
